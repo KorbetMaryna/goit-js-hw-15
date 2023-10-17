7dcc590bde177d7041c0ac0b0576e555a68f66c2
Maryna-Korbet <97749149+Maryna-Korbet@users.noreply.github.com>
2023-10-18 01:49:45 +0300

    added task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handlerSubmitForm);
+
+function handlerSubmitForm(e) {
+    e.preventDefault();
+
+    const { email, password } = e.currentTarget.elements; 
+
+    if (email.value === "" || password.value === "") {
+        alert('Attention! All the fields must be filled');
+    }
+    
+    const data = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(data);
+    e.currentTarget.reset();
+}
